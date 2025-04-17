af3d4b0b9ebae7a41a55ca781b95c43ee4c81ad0
ShanePhantom <134961313+ShanePhantom@users.noreply.github.com>
2025-04-18 03:42:05 +0530

    Update server.py

--- server.py
+++ server.py
@@ -30,5 +30,5 @@ Equation: {equation}
     return jsonify({"response": steps})
 
 if __name__ == "__main__":
-    app.run()app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
